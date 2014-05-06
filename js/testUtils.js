a5eeecb1b0c42404ddf459244852e7b0008e3be4
pwoloszun <piotr.woloszun@gmail.com>
2014-05-07 01:54:43 +0200

    improved assertion

--- js/testUtils.js
+++ js/testUtils.js
@@ -1,4 +1,13 @@
-function assertEqual(actual, expected) {
-  if (actual !== expected)
-    throw "Expected: " + JSON.stringify(expected) + ", actual: " + JSON.stringify(actual);
-}
+var assertEqual = (function() {
+
+  function stringify(obj) {
+    return JSON.stringify(obj) || ("" + obj);
+  }
+
+  function assertEqual(actual, expected) {
+    if (actual !== expected)
+      throw new Error("Expected: " + stringify(expected) + ", actual: " + stringify(actual));
+  }
+
+  return assertEqual;
+})();
