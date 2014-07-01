8ade74904bfceaffffb1438bd34030154b800a79
pwoloszun <piotr.woloszun@gmail.com>
2014-07-01 13:43:26 +0200

    adde User test

--- js/reusable_code/inheritance/User.js
+++ js/reusable_code/inheritance/User.js
@@ -5,3 +5,21 @@ function User(login) {
 User.prototype.getLogin = function() {
   return this.login;
 };
+
+function testUser() {
+  var User = extend(null, {
+    constructor: function(login) {
+      this.login = login;
+    },
+    getLogin: function() {
+      return this.login;
+    }
+  });
+
+  var login = "ed";
+  var user = new User(login, perms);
+
+  assertEqual(user.constructor, User);
+  assertEqual(user.getLogin(), login);
+}
+testUser();
