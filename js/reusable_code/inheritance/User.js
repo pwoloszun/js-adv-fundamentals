c41e9e8e0aab2d48f5288ea210e4967b52aadd30
pwoloszun <piotr.woloszun@gmail.com>
2014-07-15 14:41:46 +0200

    improved inheritance task

--- js/reusable_code/inheritance/User.js
+++ js/reusable_code/inheritance/User.js
@@ -1,13 +1,13 @@
-function testUser() {
-  var User = extend(null, {
-    constructor: function(login) {
-      this.login = login;
-    },
-    getLogin: function() {
-      return this.login;
-    }
-  });
+var User = extend(null, {
+  constructor: function(login) {
+    this.login = login;
+  },
+  getLogin: function() {
+    return this.login;
+  }
+});
 
+function testUser() {
   var login = "ed";
   var user = new User(login);
 
