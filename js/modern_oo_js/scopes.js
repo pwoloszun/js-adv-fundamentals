31298147df265ec3da7e885181755e6cc8868d46
Piotr Woloszun <piotr.woloszun@gmail.com>
2015-06-01 07:02:32 +0200

    scopes exercise

--- js/modern_oo_js/scopes.js
+++ js/modern_oo_js/scopes.js
@@ -1,6 +1,22 @@
-// Zaloguje na konsoli argumenty z jakimi funkcja zostala wywolana, każde logowanie z opoznieniem +1 sec
-function logWithDelay() {
+var xxx = "global";
+
+function outerFn() {
+  var xxx = "outerFn";
+
+  function innerFn1() {
+    var xxx = "innerFn1";
+
+    function innerFn2() {
+      var z = "innerFn2";
+    }
+
+    innerFn2();
+    console.log("After innerFn2", xxx);
+  }
+
+  innerFn1();
+  console.log("After innerFn1", xxx);
 }
 
-function emulateLocalScope() {
-}
+outerFn();
+console.log("After outerFn", xxx);
