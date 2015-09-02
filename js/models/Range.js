1ef9169cdef5a91d70f8ed17f0662ead174d9f88
Piotr Woloszun <piotr.woloszun@gmail.com>
2015-09-02 14:54:01 +0200

    impr

--- js/models/Range.js
+++ js/models/Range.js
@@ -8,22 +8,24 @@ function rangeExampleUsage() {
   var otherRange = new Range(-10, 20);
 
   /* include(number) returns true if number ranges from start to end */
-  range.include(21); // => true
-  range.include(100); // => false
+  console.log("include 21", range.include(21)); // => true
+  console.log("include 100", range.include(100)); // => false
 
   /* length() returns length of range */
-  range.length(); // => 89
+  console.log("length:", range.length()); // => 89
 
   /* intersectsith(otherRange) returns true if intersection of both ranges is not empty */
-  range.intersectsWith(otherRange); // => true
+  //range.intersectsWith(otherRange); // => true
 
   /* equal(otherRange) returns true if both ranges start and end are same */
-  range.equal(otherRange); // => false
+  //range.equal(otherRange); // => false
 
 
   /*intersection(otherRange) returns new Range instance that represents intersection of original ranges */
-  range.intersection(otherRange); // => newRange (== new Range(10, 20))
+  //range.intersection(otherRange); // => newRange (== new Range(10, 20))
 
   /* covers(otherRange) returns true if range covers otherRange */
-  range.covers(otherRange); // => false
+  //range.covers(otherRange); // => false
 }
+
+//rangeExampleUsage();
