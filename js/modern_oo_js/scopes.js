051c71a0f0d3120f592453df7439436b34d4330f
Piotr Woloszun <piotr.woloszun@gmail.com>
2016-05-10 07:35:19 +0200

    scopes task updt

--- js/modern_oo_js/scopes.js
+++ js/modern_oo_js/scopes.js
@@ -4,11 +4,11 @@ function outerFn() {
   var xxx = "outerFn";
 
   function innerFn1() {
-    xxx = "innerFn1";
+    var xxx = "innerFn1";
 
     function innerFn2() {
       var xxx = "innerFn2";
-
+      console.log("inside innerFn2", xxx);
     }
 
     innerFn2();
